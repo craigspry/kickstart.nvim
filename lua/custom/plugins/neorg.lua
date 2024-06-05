23cd4a0b971ce1d197fc5a75199d1e8a69d870e3
Craig Spry <craig.spry@gmail.com>
2024-06-05 22:24:26 +1000

    added neorg config and deleted some lines from init.lua

---
+++ lua/custom/plugins/neorg.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    'vhyrro/luarocks.nvim',
+    priority = 1000, -- We'd like this plugin to load first out of the rest
+    config = true, -- This automatically runs `require("luarocks-nvim").setup()`
+  },
+  {
+    'nvim-neorg/neorg',
+    -- build = ':Neorg sync-parsers',
+    opts = {},
+    dependencies = { 'luarocks.nvim' },
+    -- put any other flags you wanted to pass to lazy here!
+    config = function()
+      require('neorg').setup {
+        load = {
+          ['core.defaults'] = {}, -- Loads default behaviour
+          ['core.concealer'] = {}, -- Adds pretty icons to your documents
+          ['core.dirman'] = { -- Manages Neorg workspaces
+            config = {
+              workspaces = {
+                notes = '~/src/notes',
+              },
+              default_workspace = 'notes',
+            },
+          },
+        },
+      }
+    end,
+  },
+}
